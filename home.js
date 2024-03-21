95a8b9101b2b0bd23d9736e88793de0eb3acfab0
guillaumegrdi <149146710+guillaumegrdi@users.noreply.github.com>
2024-03-21 11:31:28 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,14 @@
+const li1 = document.getElementById('li-1')
+const li2 = document.getElementById('li-2')
+const li3 = document.getElementById('li-3')
+const li4 = document.getElementById('li-4')
+const li5 = document.getElementById('li-5')
+
+
+
+li1.addEventListener('click', ()=>{
+    window.scrollTo({
+        top: 1000,
+        behavior: "smooth"
+      });
+})
