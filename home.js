bdd2301cc2436c00a693ae478e4ea0bf76ca0105
guillaumegrdi <149146710+guillaumegrdi@users.noreply.github.com>
2024-04-04 15:17:38 +0200

    Add files via upload

--- home.js
+++ home.js
@@ -6,13 +6,15 @@ const li5 = document.getElementById("li-5");
 const zoombtn1 = document.getElementById("zoom-btn1");
 const imagetozoom1 = document.getElementById("imagetozoom1");
 const zoombtn2 = document.getElementById("zoom-btn2");
+const zoombtn3 = document.getElementById("zoom-btn3");
 const imagetozoom2 = document.getElementById("imagetozoom2");
 const lecteur = document.querySelector(".lecteur");
 const lecteurcontainer = document.querySelector(".lecteur-container");
 const closebtn = document.getElementById("close-btn");
 imagetozoom2.style.marginTop = "-50px";
-const blancsouris = document.getElementById('blancsouris')
-const body = document.querySelector('body')
+const blancsouris = document.getElementById("blancsouris");
+const body = document.querySelector("body");
+
 li1.addEventListener("click", (e) => {
   window.scrollTo({
     top: 400,
@@ -45,32 +47,73 @@ window.addEventListener("scroll", (e) => {
 
 zoombtn1.addEventListener("click", () => {
   lecteur.style.display = "block";
-  lecteurcontainer.innerHTML = ` <img src="Alpha_v1.1.1.webp" alt="" id="imagetozoom1" />`;
+  lecteurcontainer.innerHTML = `<img src="Alpha_v1.1.1.webp" alt="" id="imagetozoom1" />`;
 });
+
 zoombtn2.addEventListener("click", () => {
   lecteur.style.display = "block";
-  lecteurcontainer.innerHTML = ` <img src="nager.webp" alt="" id="imagetozoom2" />`;
-  imagetozoom2.style.width = "1000px";
-  imagetozoom2.style.marginTop = "50px";
+  lecteurcontainer.innerHTML = `<img src="nager.webp" alt="" id="imagetozoom2" />`;
+  imagetozoom2.style.width = "100px";
 });
 
 closebtn.addEventListener("click", () => {
   lecteur.style.display = "none";
+  imagetozoom1.style.width = "500px";
+  imagetozoom1.style.marginTop = "-20px";
   imagetozoom2.style.width = "500px";
   imagetozoom2.style.marginTop = "-50px";
 });
-window.addEventListener('mousemove', (e) =>{
-  blancsouris.style.left = e.pageX + "px"
-  blancsouris.style.top = e.pageY + "px"
+window.addEventListener("mousemove", (e) => {
+  blancsouris.style.left = e.pageX + "px";
+  blancsouris.style.top = e.pageY + "px";
   blancsouris.style.transform = "translate(-25px, -25px)";
-})
+});
 
+zoombtn3.addEventListener("click", () => {
+  lecteur.style.display = "block";
+  lecteurcontainer.innerHTML = ` <img src="Alpha_v1.1.1.webp" alt="" id="imagetozoom1" />`;
+});
 
-
-li1.addEventListener("mouseenter", () =>{
-  blancsouris.style.boxShadow = "15px 15px 500px 50px rgb(0, 255, 21"
-})
-li1.addEventListener("mouseout", () =>{
-  blancsouris.style.boxShadow = "15px 15px 500px 50px white"
-})
-
+window.addEventListener("keypress", (e) => {
+  if (e.key == "k") {
+    window.addEventListener("keypress", (e) => {
+      if (e.key == "o") {
+        window.addEventListener("keypress", (e) => {
+          if (e.key == "n") {
+            window.addEventListener("keypress", (e) => {
+              if (e.key == "a") {
+                window.addEventListener("keypress", (e) => {
+                  if (e.key == "m") {
+                    window.addEventListener("keypress", (e) => {
+                      if (e.key == "i") {
+                        window.addEventListener("keypress", (e) => {
+                          if (e.key == "c") {
+                            window.addEventListener("keypress", (e) => {
+                              if (e.key == "o") {
+                                window.addEventListener("keypress", (e) => {
+                                  if (e.key == "d") {
+                                    window.addEventListener("keypress", (e) => {
+                                      if (e.key == "e") {
+                                        window.open("index.html");
+                                        // lecteur.style.display = "block";
+                                        // lecteurcontainer.innerHTML = ` <img src="Alpha_v1.1.1.webp" alt="" id="imagetozoom1" />`;
+                                      }
+                                    });
+                                  }
+                                });
+                              }
+                            });
+                          }
+                        });
+                      }
+                    });
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+});
